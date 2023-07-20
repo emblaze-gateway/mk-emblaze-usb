6a76370b22a572a31155206925e836f5c31b297f
jhlee <jhlee@neostack.co.kr>
2023-07-20 15:13:41 +0900

    Implement features

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# mk-emblaze-usb
+# EMBLAZE-USB 생성기
+
+## Usage
+
+```sh
+$ mk-emblaze-usb.sh [DIR_PATH]
+```
+
+- `DIR_PATH`가 생략된 경우, `./data/` 디렉토리가 `DIR_PATH`로 지정되고,
+  함께 기본 세팅으로 파일들이 자동으로 생성된다.
+- `DIR_PATH`을 이미지로 생성한다.
+- dd 명령어 (linux), https://www.alexpage.de/usb-image-tool/ (window) 등 이미지 백업 툴을 이용해서
+  usb flash memory disk에 다운로드한다.
+- EMBLAZE-USB 가 설치되어있는 gateway에 USB 삽입. 부팅 후 1분 전후의 유예시간을 두고 연결하여
+  작동시킨다.
---
+++ mk-emblaze-usb.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+IMAGENAME=emblaze-usb.img
+USBLABEL=EMBLAZE-USB
+MOUNTPOINT=./img_mount_point
+DATADIR=$1
+
+if [ -e $MOUNTPOINT ]; then
+    rm -r $MOUNTPOINT
+fi
+
+if [ -z $DATADIR ]; then
+    DATADIR=./data
+fi
+
+set_default_config()
+{
+cat << EOF > $1/cmd.txt
+# specific command
+
+echo "Emblaze USB Command"
+# sleep 10s
+# reboot
+# shutdown
+EOF
+
+cat << EOF > $1/run.txt
+# emblaze-usb configuration file
+
+wifi    # wifi set
+cmd     # command
+config  # emblaze-gateway config
+EOF
+
+cat << EOF > $1/wifi.txt
+# wifi information
+
+disable      # enable or disable
+# WIFI_SSID
+# WIFI_PASSWORD
+EOF
+
+cp example.ini $1/config.ini
+}
+
+if [ ! -e $DATADIR ]; then
+    mkdir -p $DATADIR
+    set_default_config $DATADIR
+fi
+
+dd if=/dev/zero of=$IMAGENAME bs=1M count=0 seek=40
+
+mkfs.fat -F 32 -n $USBLABEL -v $IMAGENAME
+
+finish()
+{
+    sudo umount $MOUNTPOINT || true
+    echo -e "\e[31m Fail making EMBLAZE-USB\e[0m"
+    exit -1
+}
+
+echo "mkdir mountpoint"
+mkdir -p $MOUNTPOINT
+echo "mount"
+sudo mount $IMAGENAME $MOUNTPOINT
+trap finish ERR
+
+echo "copy"
+sudo cp $DATADIR/* $MOUNTPOINT
+
+echo "umount"
+sudo umount $MOUNTPOINT
+
+echo "Success making EMBLAZE-USB"
